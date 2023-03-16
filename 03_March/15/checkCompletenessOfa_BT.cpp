20d17b0a4f1379e3f944628840f7de022081bd9a
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-03-16 20:35:46 +0530

    Medium

---
+++ 03_March/15/checkCompletenessOfa_BT.cpp
@@ -0,0 +1,31 @@
+/*
+ Time: O(N)
+ Space: O(N)
+*/
+
+
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if (!root) return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool onlyLeafNow = false;
+      
+        while (q.size()) {
+            int cnt = q.size();
+         
+            while (cnt--) {
+                root = q.front();
+                q.pop();
+              
+                if (!root->left && root->right) return false;
+                if (onlyLeafNow && (root->left || root->right)) return false;
+                if (root->left) q.push(root->left);
+                if (root->right) q.push(root->right);
+                if (!root->left || !root->right) onlyLeafNow = true;
+            }
+        }
+        return true;
+    }
+};
